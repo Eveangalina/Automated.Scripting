6333ebe6815c1058029a7e25e3b75cf85c9df329
Eveangalina <eveangalinascampos@gmail.com>
2023-12-06 20:32:59 -0700

    6-python.py

---
+++ 6-python.py
@@ -0,0 +1,34 @@
+# Python
+
+# Script Name:                  Ops Challenge: Bash in Python
+# Author:                       Eveangalina Campos
+# Date of latest revision:      12/06/2023
+# Purpose:                      Ensures consistency and adherence to the Python naming conventions. 
+# Execution:                    6-python.py
+# Resource:                     
+
+
+# Import the 'os' module
+import os
+
+# Declare and initialize variables
+variable1 = "eve"
+variable2 = 27
+variable3 = True
+
+# Print the variables
+print("variable 1:", variable1)
+print("variable 2:", variable2)
+print("variable 3:", variable3)
+
+# Execute bash commands using the 'os' module
+# Command 1: whoami
+whoami_result = os.popen("whoami").read()
+print("UserLogIn: \n", whoami_result)
+
+ip_result = os.popen("ip a").read()
+print("IP addresses: \n", ip_result)
+
+# Command 3: lshw -short
+lshw_result = os.popen("sudo lshw -short").read()
+print("Hardware information:\n", lshw_result)
