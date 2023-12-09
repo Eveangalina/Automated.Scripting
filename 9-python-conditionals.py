a5de89969e9ae26ab36462ae13763383e2b9cacc
Eveangalina <eveangalinascampos@gmail.com>
2023-12-08 21:31:52 -0700

    9-python-conditionals.py

---
+++ 9-python-conditionals.py
@@ -0,0 +1,48 @@
+# Define variables a and b
+a = 5
+b = 10
+
+# Equals: a == b
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+# Not Equals: a != b
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+# Less than: a < b
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+# Less than or equal to: a <= b
+if a <= b:
+    print("a is less than or equal to b")
+else:
+    print("a is greater than b")
+
+# Greater than: a > b
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+# Greater than or equal to: a >= b
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("a is less than b")
+
+# If statement with elif and else
+c = 15
+if c < 10:
+    print("c is less than 10")
+elif c > 10:
+    print("c is greater than 10")
+else:
+    print("c is equal to 10")
