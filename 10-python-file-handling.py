c2f628dbad79dca49c899b7b0d35ad61b0bc0538
Eveangalina <eveangalinascampos@gmail.com>
2023-12-08 21:07:39 -0700

    10-python-file-handling.py

---
+++ 10-python-file-handling.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Script:                       Ops Challenge: Python File Handling
+# Author:                       Eveangalina Campos
+# Date of latest revision:      12/08/23
+# Purpose:                      File Handling in Python
+
+import os
+
+# Variable Declarations
+file_path = "my_file.txt"
+
+# Main
+def create_and_manipulate_file():
+    # Create a new .txt file and append three lines
+    with open(file_path, "w") as file:
+        file.write("First line\n")
+        file.write("Second line\n")
+        file.write("Third line\n")
+
+    # Open the file to read the first line and print it
+    with open(file_path, "r") as file:
+        first_line = file.readline()
+        print("First line:", first_line.strip())
+
+    # Delete the .txt file
+    os.remove(file_path)
+    print(f"File '{file_path}' deleted.")
+
+# Execute the main function
+if __name__ == "__main__":
+    create_and_manipulate_file()
--- 8-list-operations.py
+++ 8-list-operations.py
@@ -1,60 +1,63 @@
 def main():
     # Assign a list of ten string elements
-    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit']
+    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit', 'peach', 'peach']
 
     # Print the fourth element of the list
     print("Fourth element of the list:", my_list[3])
 
     # Print the sixth through tenth element of the list
-    print("Sixth through tenth elements of the list:", my_list[5:])
+    print("Sixth through tenth elements of the list:", my_list[5:10])
+    for fig in my_list:
+        print("bananna")
 
-    # Change the value of the seventh element to "onion"
-    my_list[6] = 'onion'
 
-    # Demonstrate using various list methods
-    my_list.append('kiwi')  # append()
-    print("List after appending 'kiwi':", my_list)
+#     # Change the value of the seventh element to "onion"
+#     my_list[6] = 'onion'
 
-    my_list.clear()  # clear()
-    print("List after clearing:", my_list)
+#     # Demonstrate using various list methods
+#     my_list.append('kiwi')  # append()
+#     print("List after appending 'kiwi':", my_list)
 
-    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit']
-    my_list_copy = my_list.copy()  # copy()
-    print("Copied list:", my_list_copy)
+#     my_list.clear()  # clear()
+#     print("List after clearing:", my_list)
 
-    count_date = my_list.count('date')  # count()
-    print("Count of 'date':", count_date)
+#     my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit']
+#     my_list_copy = my_list.copy()  # copy()
+#     print("Copied list:", my_list_copy)
 
-    my_list.extend(['kiwi', 'lemon'])  # extend()
-    print("Extended list:", my_list)
+#     count_date = my_list.count('date')  # count()
+#     print("Count of 'date':", count_date)
 
-    index_cherry = my_list.index('cherry')  # index()
-    print("Index of 'cherry':", index_cherry)
+#     my_list.extend(['kiwi', 'lemon'])  # extend()
+#     print("Extended list:", my_list)
 
-    my_list.insert(2, 'blueberry')  # insert()
-    print("List after inserting 'blueberry':", my_list)
+#     index_cherry = my_list.index('cherry')  # index()
+#     print("Index of 'cherry':", index_cherry)
 
-    popped_element = my_list.pop()  # pop()
-    print("Popped element:", popped_element)
+#     my_list.insert(2, 'blueberry')  # insert()
+#     print("List after inserting 'blueberry':", my_list)
 
-    my_list.remove('elderberry')  # remove()
-    print("List after removing 'elderberry':", my_list)
+#     popped_element = my_list.pop()  # pop()
+#     print("Popped element:", popped_element)
 
-    my_list.reverse()  # reverse()
-    print("Reversed list:", my_list)
+#     my_list.remove('elderberry')  # remove()
+#     print("List after removing 'elderberry':", my_list)
 
-    my_list.sort()  # sort()
-    print("Sorted list:", my_list)
+#     my_list.reverse()  # reverse()
+#     print("Reversed list:", my_list)
 
-    # Create a tuple
-    my_tuple = ('red', 'green', 'blue')
+#     my_list.sort()  # sort()
+#     print("Sorted list:", my_list)
 
-    # Create a set
-    my_set = {'apple', 'banana', 'cherry'}
+#     # Create a tuple
+#     my_tuple = ('red', 'green', 'blue')
 
-    # Create a dictionary
-    my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+#     # Create a set
+#     my_set = {'apple', 'banana', 'cherry'}
+
+#     # Create a dictionary
+#     my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
 
 if __name__ == "__main__":
     main()
-# resources [ChatGPT] (https://chat.openai.com/share/178aef5b-2050-4453-8336-5b4a79912950)
+# # resources [ChatGPT] (https://chat.openai.com/share/178aef5b-2050-4453-8336-5b4a79912950)
