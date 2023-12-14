74dd9c450fc34c73016aacbed3ec34c3f4cad9f2
Eveangalina <eveangalinascampos@gmail.com>
2023-12-13 19:18:03 -0700

    13powershell-AD-automation.py

--- 13powershell-AD-automation.py
+++ 13powershell-AD-automation.py
@@ -1,1 +1,8 @@
+# Creating a new user
 New-ADUser -Name "Franz Ferdinand" -Title "TPS Reporting Lead" -Department "TPS Department" -Company "GlobeX USA" -Office "Springfield, OR" -EmailAddress "ferdi@GlobeXpower.com"
+
+# Creating a new group in AD
+New-ADGroup -Name "TPSReporters" -GroupScope Global -Path "OU=Groups,DC=YourDomain,DC=com" -Description "Group for TPS Reporting team"
+
+# Creating a new OU in AD
+New-ADOrganizationalUnit -Name "TPSDepartment" -Path "DC=YourDomain,DC=com" -Description "Organizational Unit for TPS Department"
