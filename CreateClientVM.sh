f9fd3d7398648a63c9f0e1b54a1b802537369c2d
Eveangalina campos <eveangalinacampos@Eveangalinas-MacBook-Pro.local>
2025-05-12 13:40:50 -0700

    Add automated script to build Client01 VM

---
+++ CreateClientVM.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script:                       Create Client VM
+# Author:                       Eveangalina Campos
+# Date of latest revision:      05/2025
+# Purpose:                      CloudReadyUserProvisioning w Hybrid Join
+# Create the VM
+VBoxManage createvm --name "Client01" --ostype "Windows10_64" --register
+
+# Configure memory and CPU
+VBoxManage modifyvm "Client01" --memory 4096 --cpus 2
+
+# Set networking to NAT
+VBoxManage modifyvm "Client01" --nic1 nat
+
+# Create a virtual hard disk
+VBoxManage createhd --filename "$HOME/VirtualBox VMs/Client01/Client01.vdi" --size 40000
+
+# Add SATA controller
+VBoxManage storagectl "Client01" --name "SATA Controller" --add sata --controller IntelAhci
+
+# Attach the hard disk
+VBoxManage storageattach "Client01" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/Client01/Client01.vdi"
+
+# Attach the Windows 10 Education ISO
+VBoxManage storageattach "Client01" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "/Users/eveangalinacampos/Desktop/Sandbox_MSSA/Windows10Education.iso"
+
+# Start the VM
+VBoxManage startvm "Client01"
